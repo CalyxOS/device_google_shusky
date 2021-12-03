ccf28c01039bb8989871814d47ef95e3cc5eff6d
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 01:01:42 +0530

    shusky: Set up proprietary files extraction
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
    Co-authored-by: Cosmin Tanislav <demonsingur@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

--- calyx_husky.mk
+++ calyx_husky.mk
@@ -11,6 +11,7 @@ $(call inherit-product, vendor/calyx/config/common_phone.mk)
 # Inherit device configuration
 DEVICE_CODENAME := husky
 DEVICE_PATH := device/google/shusky
+VENDOR_PATH := vendor/google/husky
 $(call inherit-product, device/google/zuma/calyx_common.mk)
 $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-calyx.mk)
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
@@ -29,3 +30,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/husky/husky:14/AP2A.240905.003/12231197:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
