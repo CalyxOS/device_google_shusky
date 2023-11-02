e3954d226ec6271c870d6efd5dac33603e55cd3d
Chirayu Desai <chirayudesai1@gmail.com>
2023-11-02 23:42:09 +0530

    shusky: Add privapp permissions for PixelDisplayService
    Change-Id: I69792bbbd6844194f2098e12f3759dde805f6375

--- device-shiba.mk
+++ device-shiba.mk
@@ -18,6 +18,10 @@ endif
 # Inherit from zuma
 include device/google/zuma/device-shipping-common.mk
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/permissions_com.android.pixeldisplayservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.pixeldisplayservice.xml
+
 # EUICC
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
