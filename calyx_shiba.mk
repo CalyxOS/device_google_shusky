3f36fefbfa41c63e4bfb546e8e9acb11313479ba
Michael Bestas <mkbestas@gmail.com>
2025-03-06 03:22:57 +0200

    Update fingerprint/build description from BP1A.250305.019
    Change-Id: Ie62efde7d174d51512e684b951a5ed8226b1f4ec

--- calyx_shiba.mk
+++ calyx_shiba.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel 8
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="shiba-user 15 AP4A.250205.002 12821496 release-keys" \
-    BuildFingerprint=google/shiba/shiba:15/AP4A.250205.002/12821496:user/release-keys \
+    BuildDesc="shiba-user 15 BP1A.250305.019 13003188 release-keys" \
+    BuildFingerprint=google/shiba/shiba:15/BP1A.250305.019/13003188:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
