7d05aaa94715a79a878d7917917c9a334610d5f5
Michael Bestas <mkbestas@gmail.com>
2026-03-18 03:09:47 +0200

    Initial CalyxOS makefiles
    Change-Id: I0aeb85302967c3ce004f7f18a967813684783088

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,5 +6,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_husky.mk \
     $(LOCAL_DIR)/aosp_shiba.mk \
+    $(LOCAL_DIR)/calyx_husky.mk \
+    $(LOCAL_DIR)/calyx_shiba.mk \
     $(LOCAL_DIR)/lineage_husky.mk \
     $(LOCAL_DIR)/lineage_shiba.mk
