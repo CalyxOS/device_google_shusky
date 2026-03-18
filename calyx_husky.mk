f9d875662287ef7b24548173176c29c239bf113d
Michael Bestas <mkbestas@gmail.com>
2026-03-18 03:09:47 +0200

    Initial CalyxOS makefiles
    Change-Id: I0aeb85302967c3ce004f7f18a967813684783088

---
+++ calyx_husky.mk
@@ -0,0 +1,28 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common stuff
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Inherit device configuration
+DEVICE_CODENAME := husky
+DEVICE_PATH := device/google/shusky
+VENDOR_PATH := vendor/google/husky
+$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 8 Pro
+PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="husky-user 16 BP4A.260205.001 14624666 release-keys" \
+    BuildFingerprint=google/husky/husky:16/BP4A.260205.001/14624666:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
