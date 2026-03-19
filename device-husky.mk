68532cd0b0739893d2ef812bd92d78c7a9650a3e
Michael Bestas <mkbestas@gmail.com>
2026-03-19 23:30:14 +0200

    shusky: Remove unwanted overlays
    We don't ship the proprietary google packages for these.
    
    Change-Id: I0bd69364411626a1574afce9aabe5880972b6d51

--- device-husky.mk
+++ device-husky.mk
@@ -42,7 +42,6 @@ PRODUCT_PACKAGES += \
     SafetyRegulatoryInfoOverlayProductShusky
 
 PRODUCT_PACKAGES += \
-    DMServiceOverlayVendorHusky \
     FrameworkResOverlayProductHusky \
     FrameworkResOverlayVendorHusky \
     PixelDisplayServiceOverlayProductHusky \
