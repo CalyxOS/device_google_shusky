e2f98acb756b0201e71738e8038c5d729bb7e0c1
Michael Bestas <mkbestas@gmail.com>
2026-08-07 02:55:37 +0300

    calyx: Update to CP2A.260805.005
    Change-Id: I5dc543fe8a13c6e4d846eaefa32399df807ced1f

--- calyx_husky.mk
+++ calyx_husky.mk
@@ -20,9 +20,9 @@ PRODUCT_SYSTEM_MANUFACTURER := Google
 PRODUCT_SYSTEM_NAME := generic_system_google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="husky-user 17 CP2A.260705.006 15641320 release-keys" \
-    BuildFingerprint=google/husky/husky:17/CP2A.260705.006/15641320:user/release-keys \
-    BuildSystemFingerprint=google/generic_system_google/generic:17/CP2A.260705.006/15641320:user/release-keys \
+    BuildDesc="husky-user 17 CP2A.260805.005 15828068 release-keys" \
+    BuildFingerprint=google/husky/husky:17/CP2A.260805.005/15828068:user/release-keys \
+    BuildSystemFingerprint=google/generic_system_google/generic:17/CP2A.260805.005/15828068:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
